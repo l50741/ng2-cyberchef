b1cf13310bcf483d01720f58aaadcf710815d728
l50741 <l50741@users.noreply.github.com>
2017-04-21 12:44:14 +0100

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CyberChef
 
+Demo: (https://l50741.github.io/ng2-cyberchef/)
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0.
 
 ## Development server
@@ -17,12 +19,3 @@ Run `ng build` to build the project. The build artifacts will be stored in the `
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-Before running the tests make sure you are serving the app via `ng serve`.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { Ng2Webstorage } from 'ng2-webstorage';
 import { DragulaModule } from 'ng2-dragula';
-import { ConfigModule, ConfigLoader, ConfigStaticLoader } from 'ng2-config';
 
 import { AppComponent } from './app.component';
 import { OperationsComponent } from './operations/operations.component';
@@ -14,10 +13,6 @@ import { RecipeComponent } from './recipe/recipe.component';
 import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
 import { ControlsComponent } from './controls/controls.component';
-
-export function configFactory() {
-    return new ConfigStaticLoader('/config.json');
-}
 
 @NgModule({
   declarations: [
@@ -38,11 +33,7 @@ export function configFactory() {
     ReactiveFormsModule,
     HttpModule,
     MaterialModule,
-    Ng2Webstorage,
-    ConfigModule.forRoot({
-      provide: ConfigLoader,
-      useFactory: (configFactory)
-    })
+    Ng2Webstorage
   ],
   providers: [],
   bootstrap: [AppComponent]
